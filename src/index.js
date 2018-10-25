0f78e5d4a5e4c549ddf2a0cd4d4a60c20df3a1b4
Ellen Choi <ellenchoi98@outlook.com>
2018-10-24 21:36:37 -0400

    Added a searchbox to filter out the cards

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card.js';
+import App from './App';
 import CardList from './CardList';
 import 'tachyons';
-import {robots} from './robots.js';
 import * as serviceWorker from './serviceWorker';
 
 
-ReactDOM.render(
-	<CardList robots={robots}/>
-
-	, document.getElementById('root'));
+ReactDOM.render(<App/>,document.getElementById('root'));
