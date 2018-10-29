c34fe449685d305d8c5eaeba978db95fdecf8c69
Ellen Choi <ellenchoi98@outlook.com>
2018-10-29 19:12:05 -0400

    Replaced js file with a json html, added functionality where the search bar stays at the top

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import CardList from './CardList.js';
-import {robots} from './robots.js';
 import SearchBox from './SearchBox.js';
 import './App.css';
+import Scroll from "./Scroll";
 
 class App extends Component {
 	constructor() {
 		super()
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchfield: ''
 
 		}
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response=> response.json())
+			.then(users=> this.setState({ robots: users}));	
 	}
 
 	onSearchChange = (event) => {
@@ -23,13 +29,21 @@ class App extends Component {
 			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
 		})
 
+		if (this.state.robots.length == 0){
+			return <h1>Loading</h1>
+		}
+		else{
+
 		return (
 			<div className = "tc">
 				<h1 className='f1'>RoboFriends</h1>
 				<SearchBox searchChange={this.onSearchChange}/>
-				<CardList robots={filteredRobots}/>
+				<Scroll>
+					<CardList robots={filteredRobots}/>
+				</Scroll>
 			</div>
 		);
+	}
 
 	}
 }
